0ad5c33bf1ddbcb236237c74c639dbbee9edca29
Batuhan18 <54512066+Batuhan18@users.noreply.github.com>
2024-11-19 22:26:38 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,84 @@
+﻿using Antlr.Runtime.Tree;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var mail = (string)Session["CariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            return View(degerler);
+        }
+
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.Cariid).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
+            return View(degerler);
+        }
+
+        public ActionResult GelenMesajlar()
+        {
+            var mail = (string)Session["CariMail"];
+            var mesajlar = c.mesajlars.Where(x => x.Alici == mail).OrderByDescending(x=>x.MesajID).ToList();
+            var gelensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
+            return View(mesajlar);
+        }
+        public ActionResult GidenMesajlar()
+        {
+            var mail = (string)Session["CariMail"];
+            var mesajlar = c.mesajlars.Where(x => x.Kimden == mail).OrderByDescending(z => z.MesajID).ToList();
+            var gelensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
+            return View(mesajlar);
+        }
+
+        public ActionResult MesajDetay(int id)
+        {
+            var degerler = c.mesajlars.Where(x => x.MesajID == id).ToList();
+            var mail = (string)Session["CariMail"];
+            var gelensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
+            return View(degerler);
+        }
+
+
+        [HttpGet]
+        public ActionResult YeniMesaj()
+        {
+            var mail = (string)Session["CariMail"];
+            var gelensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d2 = gidensayisi;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMesaj(mesajlar m)
+        {
+            var mail = (string)Session["CariMail"];
+            m.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            m.Kimden = mail;
+            c.mesajlars.Add(m);
+            c.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        Context c = new Context();
+        public ActionResult Index(string p)
+        {
+            var urunler = from x in c.Uruns select x;
+            if (!string.IsNullOrEmpty(p))
+            {
+                urunler = urunler.Where(y => y.UrunAd.Contains(p));
+            }
+            return View(urunler.ToList());
+        }
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.KategoriAdi,
+                                               Value = x.KategoriID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(Urunler p)
+        {
+            c.Uruns.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunSil(int id)
+        {
+            var sil = c.Uruns.Find(id);
+            sil.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.KategoriAdi,
+                                               Value = x.KategoriID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var urundeger = c.Uruns.Find(id);
+            return View("UrunGetir", urundeger);
+        }
+
+        public ActionResult UrunGuncelle(Urunler p)
+        {
+            var urn = c.Uruns.Find(p.Urunid);
+            urn.AlisFiyati = p.AlisFiyati;
+            urn.Durum = p.Durum;
+            urn.Kategoriid = p.Kategoriid;
+            urn.Marka = p.Marka;
+            urn.SatisFiyati = p.SatisFiyati;
+            urn.Stok = p.Stok;
+            urn.UrunAd = p.UrunAd;
+            urn.UrunGorsel = p.UrunGorsel;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunListesi()
+        {
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult SatisYap(int id)
+        {
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }).ToList();
+            ViewBag.dgr3 = deger3;
+            var deger1 = c.Uruns.Find(id);
+            ViewBag.dgr1 = deger1.Urunid;
+            ViewBag.dgr2 = deger1.SatisFiyati;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SatisYap(SatisHareket satisHareket)
+        {
+            satisHareket.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(satisHareket);
+            c.SaveChanges();
+            return RedirectToAction("Index","Satis");
+           
+        }
+    }
+}
